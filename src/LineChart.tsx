cf7448124e7df0095a507881a70a28186f7d5d0f
rkonings <41450820+rkonings@users.noreply.github.com>
2020-09-05 15:55:07 +0200

    feat(chart): add line chart component (#13)
    * add line chart stories
    
    * add line chart component

---
+++ src/LineChart.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+import * as d3 from 'd3';
+import { getDomain } from './getDomain';
+import { AxisBottom } from './AxisBottom';
+import { AxisLeft } from './AxisLeft';
+import { Grid } from './Elements/Grid';
+import { Line } from './LineChart/Line';
+
+export interface Data {
+    name: string;
+    salary: number;
+}
+
+export interface Chart {
+    data: Data[];
+    className?: string;
+    width?: number;
+    height?: number;
+}
+
+interface InnerChart {
+    x: number;
+}
+const InnerChart = styled.div<InnerChart>`
+    ${({ x }) => `
+        position:absolute;
+        left: ${x}px;
+        width: 100%;
+        height: 100%;
+    `}
+`;
+
+const Chart = styled(
+    ({ className, data, width = 400, height = 400 }: Chart) => {
+        const scaleLinearDomain = getDomain(data);
+
+        const amountTicks = Math.ceil(width / 100);
+
+        const scaleBand = d3
+            .scaleBand()
+            .domain(data.map(({ name }) => name))
+            .padding(0.1)
+            .range([0, width]);
+
+        const scaleLinear = d3
+            .scaleLinear()
+            .domain(scaleLinearDomain)
+            .nice(amountTicks)
+            .range([height, 0]);
+
+        return (
+            <div className={className}>
+                <InnerChart x={100}>
+                    <Grid
+                        scale={scaleLinear}
+                        amountTicks={amountTicks}
+                        layout="VERTICAL"
+                    />
+                    {data.length > 0 && (
+                        <Line
+                            scaleBand={scaleBand}
+                            scaleLinear={scaleLinear}
+                            data={data}
+                        />
+                    )}
+                    <AxisBottom scale={scaleBand} amountTicks={amountTicks} />
+                </InnerChart>
+
+                <AxisLeft scale={scaleLinear} amountTicks={amountTicks} />
+            </div>
+        );
+    }
+)`
+    ${({ width = 400, height = 400 }: Chart) => `
+      width: ${width}px;
+      height: ${height}px;
+      position: relative;
+    `}
+`;
+
+export default Chart;
--- stories/chart.stories.tsx
+++ stories/chart.stories.tsx
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { boolean, number, select, text } from '@storybook/addon-knobs';
 import ChartDiv from '../src/Chart';
 import BarChartVertical from '../src/BarChartVertical';
+import LineChart from '../src/LineChart';
 import faker from 'faker';
 
 const data = Array(10)
@@ -191,4 +192,113 @@ storiesOf('Bar chart/Vertical', module)
         ];
 
         return <BarChartVertical width={width} height={height} data={data} />;
+    });
+storiesOf('Line chart', module)
+    .add('Default', () => {
+        const width = number('width', 400);
+        const height = number('height', 400);
+
+        const data = Array(3)
+            .fill(null)
+            .map(() => {
+                return {
+                    name: faker.name.firstName(),
+                    salary: faker.random.number(1000) + 100,
+                };
+            });
+
+        return <LineChart width={width} height={height} data={data} />;
+    })
+    .add('Large labels', () => {
+        const width = number('width', 800);
+        const height = number('height', 600);
+
+        const data = Array(6)
+            .fill(null)
+            .map(() => {
+                return {
+                    name: faker.internet.url(),
+                    salary: faker.random.number(1000000) + 100,
+                };
+            });
+
+        return <LineChart width={width} height={height} data={data} />;
+    })
+    .add('Negative values', () => {
+        const width = number('width', 400);
+        const height = number('height', 400);
+
+        const data = [
+            { name: 'FOOBAZ', salary: -5 },
+            { name: 'FOO', salary: -20 },
+            { name: 'BAZ', salary: -10 },
+        ];
+
+        return <LineChart width={width} height={height} data={data} />;
+    })
+    .add('Mixed values', () => {
+        const width = number('width', 400);
+        const height = number('height', 400);
+
+        const data = [
+            {
+                name: 'FOO',
+                salary: -3000,
+            },
+            {
+                name: 'BAZ',
+                salary: -5000,
+            },
+            {
+                name: 'FOOBAZ',
+                salary: 1000,
+            },
+            {
+                name: 'BAZFOO',
+                salary: 5000,
+            },
+        ];
+
+        return <LineChart width={width} height={height} data={data} />;
+    })
+    .add('Date values', () => {
+        const width = number('width', 400);
+        const height = number('height', 400);
+
+        const data = [
+            {
+                name: '1-1-2020',
+                salary: 10000,
+            },
+            {
+                name: '1-2-2020',
+                salary: 11000,
+            },
+            {
+                name: '1-3-2020',
+                salary: 1000,
+            },
+            {
+                name: '1-4-2020',
+                salary: 5000,
+            },
+            {
+                name: '1-5-2020',
+                salary: 1000,
+            },
+            {
+                name: '1-6-2020',
+                salary: 5000,
+            },
+            {
+                name: '1-7-2020',
+                salary: 1000,
+            },
+            {
+                name: '1-8-2020',
+                salary: 5000,
+            },
+        ];
+
+        return <LineChart width={width} height={height} data={data} />;
     });
